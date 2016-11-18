bc74e63a7297f5d5583e8a29149e748655a9bf78
яд <student@KIRP-405-STUD08>
2016-11-18 12:53:17 +0300

    база данных

---
+++ Cars.Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Data
+{
+    class Context:DbContext
+    {
+        public DbSet<Manufactures> Manuf { get; set; }
+        public DbSet<Model> Mode { get; set; }
+        public DbSet<Car> Cara { get; set; }
+    }
+}
