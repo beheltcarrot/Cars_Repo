f2f77f6448421eba465b3c65964e731bb43c3551
Женя <student@KIRP-405-STUD09>
2016-11-18 12:41:52 +0300

    added 3 classes

---
+++ Cars.Data/Manufactures.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Data
+{
+    class Manufactures
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public List<Model> Models { get; set; }
+    }
+}
